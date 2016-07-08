1915a7d637bb202c9ca8308c3e8beb582defaacd
Krish Ravindranath <krish.ravindranath@gmail.com>
2016-07-08 04:08:05 -0400

    Updated to fix buffer/ stack overrun issue, added comments for debugging

--- br/br.cpp
+++ br/br.cpp
@@ -26,7 +26,11 @@ static const int SERVER_PORT = 7001; //default port for TCP Axis Neuron server a
 
 static void frameDataReceived(void * customObject, SOCKET_REF sockRef, BvhDataHeader * header, float * data)
 {
+	cout << "frameDataReceived" << "\n";
 	BVH_Collector *self = reinterpret_cast<BVH_Collector*>(customObject);
+	//while (!_kbhit){
+//		cout << data[15];
+	//}
 	self->showBvhBoneInfo(sockRef, header, data);
 }
 
@@ -34,55 +38,67 @@ static void frameDataReceived(void * customObject, SOCKET_REF sockRef, BvhDataHe
 BVH_Collector::BVH_Collector(){
 	//sockTCPRef = NULL;
 	//sockUDPRef = NULL;
+	cout << "BVH_Collector" << "\n";
 	BRRegisterFrameDataCallback(this, FrameDataReceived(frameDataReceived));
 }
 
 BVH_Collector::~BVH_Collector() {
+	cout << "~BVH_Collector" << "\n";
 	BRCloseSocket(mSocketRef);
 }
 
 void BVH_Collector::testConnection()
 {
 
+	cout << "testConnection" << "\n";
 	BRCloseSocket(mSocketRef);
 
 	mSocketRef = BRConnectTo((char*)SERVER_ADDRESS.c_str(), SERVER_PORT);
 
 	if (mSocketRef) {
-		cout << "connected";
+		cout << "connected" << "\n";
 	}
 	else {
-		cout << "disconnected";
+		cout << "disconnected" << "\n";
 	}
 
 	SocketStatus status = BRGetSocketStatus(mSocketRef);
 	cout << "status" << status;
+	cout << "\n";
 }
 
 void BVH_Collector::showBvhBoneInfo(SOCKET_REF sender, BvhDataHeader* header, float* data)
 {
-	cout << "showBVHBoneInfo";
+	cout << "showBVHBoneInfo" << "\n";
+	
+	//char strFrameIndex[60];
+
 	int dataIndex = 0;
-    /** The number 15 is right fore arm // Ref:: Appendix A: Skeleton Data Sequence in Array From "Neuron Data Reader Runtime API Documentation" */
-    int currentBoneNumber = 15;
-    if (header->WithDisp)
-    {
-        dataIndex = currentBoneNumber * 6;
-        if (header->WithReference)
-        {
-            dataIndex += 6;
-        }
+	/** The number 15 is right fore arm // Ref:: Appendix A: Skeleton Data Sequence in Array From "Neuron Data Reader Runtime API Documentation" */
+	int currentBoneNumber = 15;
+	if (header->WithDisp)
+	{
+		dataIndex = currentBoneNumber * 6;
+		if (header->WithReference)
+		{
+			dataIndex += 6;
+		}
 
-        float angleX = data[dataIndex + 4];
-        float angleY = data[dataIndex + 3];
-        float angleZ = data[dataIndex + 5];
+		float angleX = data[dataIndex + 4];
+		float angleY = data[dataIndex + 3];
+		float angleZ = data[dataIndex + 5];
 
-        cout << "Angle X" << angleX << ", Angle Y" << angleY << ", Angle Z" << angleZ;
-    }
+		char strBuff[32];
+		sprintf_s(strBuff, sizeof(strBuff), "%0.3F", angleX);
+		sprintf_s(strBuff, sizeof(strBuff), "%0.3F", angleY);
+		sprintf_s(strBuff, sizeof(strBuff), "%0.3F", angleZ);
+
+		cout << "Angle X" << angleX << ", Angle Y" << angleY << ", Angle Z" << angleZ << "\n";
+	}
 }
 
 int main() {
-	cout << "start";
+	cout << "main" << "\n";
 	//BVH_Collector* b = new BVH_Collector();
 	//new BVH_Collector b;
 	//b.testConnection();
@@ -90,84 +106,89 @@ int main() {
 	//cin.get();
 
 	BVH_Collector b;
+	b.testConnection();
 
 	//while (!_kbhit()) {
-		b.testConnection();
-		cout << "testing";
-		cout << "\n";
+	//b.testConnection();
+	//cout << "testing";
+	//cout << "\n";
+	//BRCloseSocket(b.mSocketRef);
+	//_sleep(1000);
 		cin.get();
 	//}
 
 	BRCloseSocket(b.mSocketRef);
-	return 0;
+	//_sleep(500);
+	//cin.get();
+	//return 0;
 }
 
 /*int main()
 {
-	ofstream myFile;
-	myFile.open("example.csv");
-	BVH_Collector b;
-	// connect TCP
-	b.mSocketRef = BRConnectTo("127.0.0.1", 7005);
+ofstream myFile;
+myFile.open("example.csv");
+BVH_Collector b;
+// connect TCP
+b.mSocketRef = BRConnectTo("127.0.0.1", 7005);
 
-	if (b.mSocketRef) {
-		//		//print("Successfully connected via TCP");
-		cout << "Successfully connected via TCP";
-	}
-	else {
-		//print("TCP connection failed");
-		cout << "TCP connection failed";
-	}
+if (b.mSocketRef) {
+//		//print("Successfully connected via TCP");
+cout << "Successfully connected via TCP";
+}
+else {
+//print("TCP connection failed");
+cout << "TCP connection failed";
+}
 
-	/*	// connect UDP
-		b.sockUDPRef = BRStartUDPServiceAt(7001);
+/*	// connect UDP
+b.sockUDPRef = BRStartUDPServiceAt(7001);
 
-		if (b.sockUDPRef) {
-			//print("Successfully connected via UDP");
-			cout << "Successfully connected via UDP";
-		}
-		else {
-			//print("UDP connection failed");
-			cout << "UDP connection failed";
-		}
-	*/
+if (b.sockUDPRef) {
+//print("Successfully connected via UDP");
+cout << "Successfully connected via UDP";
+}
+else {
+//print("UDP connection failed");
+cout << "UDP connection failed";
+}
+*/
 
 
-	// close TCP socket
+// close TCP socket
 //	BRCloseSocket(b.sockTCPRef);
 
-	// close UDP socket
+// close UDP socket
 //	BRCloseSocket(b.sockUDPRef);
 
 /*	while (!_kbhit()) {
-	SocketStatus status = BRGetSocketStatus(b.mSocketRef);
-	cout << "status" << status << "\n";
-	
-	}
-	//cin.get();
-	BRCloseSocket(b.mSocketRef);
-	myFile.close();
-	//return 0;
+SocketStatus status = BRGetSocketStatus(b.mSocketRef);
+cout << "status" << status << "\n";
+
+}
+//cin.get();
+BRCloseSocket(b.mSocketRef);
+myFile.close();
+//return 0;
 }*/
 
 /*void __stdcall BVH_Collector::bvhFrameDataReceived(void* customedObj, SOCKET_REF sender, BvhDataHeader* header, float* data) {
-	BVH_Collector* pthis = (BVH_Collector*)customedObj;
-	//pthis->showBvhBoneInfo(sender, header, data);
+BVH_Collector* pthis = (BVH_Collector*)customedObj;
+//pthis->showBvhBoneInfo(sender, header, data);
 }*/
 
 //void __stdcall BVH_Collector::showBvhBoneInfo(SOCKET_REF sender, BvhDataHeader* header, float* data) {
-	//myFile << data[37 + 4];
+//myFile << data[37 + 4];
 //	myFile << ",";
 
-	/*
-	char strBvhData[64];
-	sprintf_s(strBvhData, "%0.3f\t", dispX);
-	sprintf_s(strBvhData, "%0.3f\t", dispY);
-	sprintf_s(strBvhData, "%0.3f\t", dispZ);
+/*
+char strBvhData[64];
+sprintf_s(strBvhData, "%0.3f\t", dispX);
+sprintf_s(strBvhData, "%0.3f\t", dispY);
+sprintf_s(strBvhData, "%0.3f\t", dispZ);
 
-	sprintf_s(strBvhData, "%0.3f\t", angX);
-	sprintf_s(strBvhData, "%0.3f\t", angY);
-	sprintf_s(strBvhData, "%0.3f\t", angZ);
-	*/
+sprintf_s(strBvhData, "%0.3f\t", angX);
+sprintf_s(strBvhData, "%0.3f\t", angY);
+sprintf_s(strBvhData, "%0.3f\t", angZ);
+*/
 
 //}
